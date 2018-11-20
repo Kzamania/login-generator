d157002c3fb4143c3b11e89f5d0688c666664e12
Maxime DURAND <maxime.durand1@univ-tlse3.fr>
2018-11-20 10:25:57 +0100

    third commit create unit test class(LoginServiceTest) delete AppTest (useless)

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,24 @@
+package geco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceTest {
+
+    @Test
+    public void loginExists() {
+    }
+
+    @Test
+    public void addLogin() {
+    }
+
+    @Test
+    public void findAllLoginsStartingWith() {
+    }
+
+    @Test
+    public void findAllLogins() {
+    }
+}
